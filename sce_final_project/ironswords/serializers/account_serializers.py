4f386bcf037b5a5ef6b0e3a34e5fb21a88963b20
ShilatTsfoni <shilattsfoni1@gmail.com>
2024-03-20 14:30:31 +0200

    fix cities list from cities_api and saving user data

--- sce_final_project/ironswords/serializers/account_serializers.py
+++ sce_final_project/ironswords/serializers/account_serializers.py
@@ -1,6 +1,8 @@
 from ironswords.models.user_model import verify_otp
 from rest_framework import serializers
 from ironswords.models.user_model import User
+from django.conf import settings
+
 class PhoneSerializer(serializers.Serializer):
     phone = serializers.CharField(max_length=10)
     def validate_phone(self,value):
@@ -34,7 +36,22 @@ class OTPSerializer(serializers.Serializer):
         else:
             raise serializers.ValidationError("OTP incorrect or timed out")
         
+ #added for cities
+class CityField(serializers.CharField): 
+    def __init__(self, **kwargs): #initialize the CityField
+        super().__init__(**kwargs)
+        self.cities = settings.CITY_LIST #load cities data when initializing the field
+
+    def to_internal_value(self,data): #validate the provided city name
+        if data not in self.cities: #check if the provided city name is in the loaded cities list
+            raise serializers.ValidationError("Invalid city")
+        return data
+    
+    def refresh_cities(self): #refresh the list of cities
+        self.cities = settings.CITY_LIST  
+
 class PrefrencesSerializer(serializers.ModelSerializer):
+    #city = CityField() #use the custom CityField for the 'city' field
     class Meta:
         model = User
         fields = ['first_name','last_name','email','gender','birth_day','city','volunteer_frequency','volunteer_categories','most_important','allow_notifications','finished_onboarding']
