f3533a47f7893ddee788854a0b23c9043cda3d7c
ShilatTsfoni <shilattsfoni1@gmail.com>
2024-03-27 23:34:34 +0200

    adding OrganizationSerializer

--- sce_final_project/ironswords/serializers/event_serializer.py
+++ sce_final_project/ironswords/serializers/event_serializer.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 from ironswords.models.user_model import User
 from ironswords.models.event_model import Event,Application
+from ironswords.models.organization_model import Organization
 
+class OrganizationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Organization
+        fields = ['name', 'description', 'contact_name', 'contact_phone']
 
 class EventSerializer(serializers.ModelSerializer):
+    organization = OrganizationSerializer()  # Include organization details in the event serializer
     class Meta:
         model = Event
         fields = "__all__"
